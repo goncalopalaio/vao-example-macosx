7272f3b94f080662308bf3df8d8fee76d448ae64
Goncalo Palaio @ Home <goncalopalaio@gmail.com>
2020-01-02 23:59:44 +0000

    working example - renders a textured quad

--- main.cpp
+++ main.cpp
@@ -12,9 +12,20 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+// For glGenVertexArrays in OSx
+#ifdef __APPLE__
+#include <OpenGL/gl3.h>
+#include <OpenGL/gl3ext.h>
+#endif
+
 
 #define WINDOW_W 600
 #define WINDOW_H 600
+
+
+// Rendering based on:
+// https://github.com/raysan5/raylib/blob/dc6136e820a5793fb469e78154b57a46ac619139/src/rlgl.h
+
 
 void close_callback(GLFWwindow * window) {
     printf("close_callback");
@@ -92,7 +103,7 @@ GLuint compile_shader(GLenum type, const char *src) {
         glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLogLength);
         GLchar* strInfoLog = (GLchar*) malloc(infoLogLength * sizeof(char));
         glGetShaderInfoLog(shader, infoLogLength, NULL, strInfoLog);
-        printf("Compilation error in shader %s\n", strInfoLog);
+        printf("\nCompilation error in shader %s\n", strInfoLog);
         free(strInfoLog);
         glDeleteShader(shader);
         return 0;
@@ -101,26 +112,22 @@ GLuint compile_shader(GLenum type, const char *src) {
     return shader;
 }
 
-inline float *push_v4_arr(float *v, float x, float y, float z, float a) {
+inline float *push_v3_arr(float *v, float x, float y, float z) {
     v[0] = x;
     v[1] = y;
     v[2] = z;
-    v[3] = a;
-    printf("push_v4_arr: %f, %f, %f, %f\n",  x, y, z, a);
-    return v + 4;
+    printf("push_v3_arr: %f, %f, %f\n",  x, y, z);
+    return v + 3;
 }
 
-inline float *push_v5_arr(float *v, float x, float y, float z, float s, float t) {
+inline float *push_v2_arr(float *v, float x, float y) {
     v[0] = x;
     v[1] = y;
-    v[2] = z;
-    v[3] = s;
-    v[4] = t;
-    printf("push_v5_arr: %f, %f, %f, %f, %f\n",  x, y, z, s, t);
-    return v + 5;
+    printf("push_v2_arr: %f, %f\n",  x, y);
+    return v + 2;
 }
 
-float* push_textured_quad_arr(float*v, float x0, float y0, float x1, float y1, float s0, float t0, float s1, float t1) {
+void push_textured_quad_arr(float*v, float* t, float x0, float y0, float x1, float y1, float s0, float t0, float s1, float t1) {
     // quad 1 2 3 4
     // -->
     // triangle 1 2 3
@@ -131,19 +138,18 @@ float* push_textured_quad_arr(float*v, float x0, float y0, float x1, float y1, f
     //v = push_v5_arr(v, x1, y1, 0, s1, t1); // 3
     //v = push_v5_arr(v, x0, y1, 0, s0, t1); // 4
 
-    v = push_v5_arr(v, x0, y0, 1.0, s0, t0); // 1
-    v = push_v5_arr(v, x1, y0, 1.0, s1, t0); // 2
-    v = push_v5_arr(v, x1, y1, 1.0, s1, t1); // 3
-
-    v = push_v5_arr(v, x1, y1, 1.0, s1, t1); // 3
-    v = push_v5_arr(v, x0, y1, 1.0, s0, t1); // 4
-    v = push_v5_arr(v, x0, y0, 1.0, s0, t0); // 1
-
-    return v;
-}
-
-float* push_textured_quad_scaled_arr(float*v, float x0, float y0, float x1, float y1, float s0, float t0, float s1, float t1, float scale_x, float scale_y) {
-    return push_textured_quad_arr(v, x0 * scale_x, y0 * scale_y, x1 * scale_x, y1 * scale_y, s0, t0, s1, t1);
+    v = push_v3_arr(v, x0, y0, 1.0);
+    t = push_v2_arr(t, s0, t0); // 1
+    v = push_v3_arr(v, x1, y0, 1.0);
+    t = push_v2_arr(t, s1, t0); // 2
+    v = push_v3_arr(v, x1, y1, 1.0);
+    t = push_v2_arr(t, s1, t1); // 3
+    v = push_v3_arr(v, x1, y1, 1.0);
+    t = push_v2_arr(t, s1, t1); // 3
+    v = push_v3_arr(v, x0, y1, 1.0);
+    t = push_v2_arr(t, s0, t1); // 4
+    v = push_v3_arr(v, x0, y0, 1.0);
+    t = push_v2_arr(t, s0, t0); // 1
 }
 
 void set_float3(float3 *v, float x, float y, float z) {
@@ -250,7 +256,13 @@ int main(int argc, char const *argv[]) {
     else {
         printf("Glfw initialized");
     }
-    window = glfwCreateWindow(WINDOW_W, WINDOW_H, "stb-true-type-demo", NULL, NULL);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(WINDOW_W, WINDOW_H, "vao-example", NULL, NULL);
     if(!window) {
         printf("Create window failed");
         glfwTerminate();
@@ -268,7 +280,9 @@ int main(int argc, char const *argv[]) {
     printf("Setting context");
     glfwMakeContextCurrent(window);
 
-	char vs_source[] =
+
+    /*
+    char vs_source_es2[] =
         "attribute vec4 position;"
         "attribute vec2 uvs;"
         "uniform mat4 view_matrix;"
@@ -279,18 +293,45 @@ int main(int argc, char const *argv[]) {
         "gl_Position =  projection_matrix * view_matrix * position;"
         "}";
 
-    char fs_source[] =
+    char fs_source_es2[] =
         "varying vec2 vuvs;"
-		"uniform sampler2D texture;"
+        "uniform sampler2D texture;"
         "void main() {"
         "vec4 c = texture2D(texture, vuvs);"
         "gl_FragColor = c;"
+        "}";
+    */
+
+	char vs_source[] =
+        "#version 330 core\n"
+        ""
+        "layout(location = 0) in vec3 position;\n"
+        "layout(location = 1) in vec2 uvs;\n"
+        "uniform mat4 view_matrix;\n"
+        "uniform mat4 projection_matrix;\n"
+        "out vec2 vuvs;\n"
+        "void main(){"
+        "vuvs = uvs;\n"
+        "gl_Position =  projection_matrix * view_matrix * vec4(position, 1.0);\n"
+        "}";
+
+    char fs_source[] =
+        "#version 330 core\n"
+        ""
+        "in vec2 vuvs;\n"
+		"uniform sampler2D texture_unit;\n"
+        "out vec4 frag_color;"
+        "void main() {\n"
+        "vec4 c = texture(texture_unit, vuvs);\n"
+        "// vec4 c = vec4(1.0,0.0,0.0,1.0);\n"
+        "frag_color = c;\n"
         "}";
 	printf("Compiling shader\n");
     GLuint main_shader = compile_shader_program(vs_source,
     													  fs_source,
     													  "position", "color");
 
+    GL_ERR;
 
 	printf("Setting up camera\n");
     float3 camera_position;
@@ -311,13 +352,17 @@ int main(int argc, char const *argv[]) {
 	m_mat4_lookat(view_matrix, &camera_position, &camera_direction, &camera_up);
 
 	printf("Creating mesh\n");
-	int vertex_data_size = sizeof(float) * 6 * 5;
-	float* vertex_data = (float *) malloc(vertex_data_size);
+    int mesh_vertices_count = 6;
+	int mesh_vertices_size = sizeof(float) * mesh_vertices_count * 3;
+    int mesh_texcoords_size = sizeof(float) * mesh_vertices_count * 2;
+	float* mesh_vertices = (float *) malloc(mesh_vertices_size);
+    float* mesh_texcoords = (float *) malloc(mesh_texcoords_size);
 
-	float* buf = vertex_data;
+	float* mv = mesh_vertices;
+    float* mt = mesh_texcoords;
     {
 		
-        buf = push_textured_quad_arr(buf, -10, -10, 10, 10, 0, 0, 1, 1);
+        push_textured_quad_arr(mv, mt, -10, -10, 10, 10, 0, 0, 1, 1);
 		// float scale = 1.0;
         //buf = push_v5_arr(buf, -scale, scale, 0.0, 0.0, 1.0);
 		//buf = push_v5_arr(buf, scale, -scale, 0.0, 1.0, 0.0);
@@ -327,18 +372,17 @@ int main(int argc, char const *argv[]) {
 		//buf = push_v5_arr(buf, scale, -scale, 0.0, 1.0, 0.0);
     }
 
-    
-	glEnable(GL_TEXTURE_2D);	
+	// glEnable(GL_TEXTURE_2D); GL_ERR; // Not applicable in gl 3.3 core
 
 	printf("Loading font texture from file\n");
-	GLuint font_texture_from_file = 0;
+	GLuint placeholder_texture = 0;
 	{
 		int width, height, channels;
 	    stbi_set_flip_vertically_on_load(true);
 	    unsigned char *pixels = stbi_load("placeholder_texture.png", &width, &height, &channels, 0);
 	    assert(pixels != NULL);
 
-        font_texture_from_file = upload_new_texture(width, height, channels, pixels);
+        placeholder_texture = upload_new_texture(width, height, channels, pixels);
 
 	    stbi_image_free(pixels);
 
@@ -353,6 +397,47 @@ int main(int argc, char const *argv[]) {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+
+    GL_ERR;
+
+    // Load mesh
+
+    bool vao_supported = true;
+
+    unsigned int mesh_vaoId = 0;
+    unsigned int mesh_vboId[2];
+    int drawHint = GL_STATIC_DRAW;
+
+    if (vao_supported) {
+        glGenVertexArrays(1, &mesh_vaoId);
+        glBindVertexArray(mesh_vaoId);
+    }
+
+    // Enable vertex attributes: position (shader-location = 0)
+    glGenBuffers(1, &mesh_vboId[0]);
+    glBindBuffer(GL_ARRAY_BUFFER, mesh_vboId[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*3*mesh_vertices_count, mesh_vertices, drawHint);
+    glVertexAttribPointer(0, 3, GL_FLOAT, 0, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    // Enable vertex attributes: texcoords (shader-location = 1)
+    glGenBuffers(1, &mesh_vboId[1]);
+    glBindBuffer(GL_ARRAY_BUFFER, mesh_vboId[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*2*mesh_vertices_count, mesh_texcoords, drawHint);
+    glVertexAttribPointer(1, 2, GL_FLOAT, 0, 0, 0);
+    glEnableVertexAttribArray(1);
+
+    if (vao_supported) {
+        if (mesh_vaoId > 0) {
+            printf("Mesh uploaded successfully to VRAM (GPU) %i\n", mesh_vaoId);
+        } else {
+            printf("ERROR: Mesh could not be uploaded to VRAM (GPU)");
+        }
+    }
+    GL_ERR;
+    //
+
+
     while(!glfwWindowShouldClose(window)) {
 		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 		
@@ -360,30 +445,45 @@ int main(int argc, char const *argv[]) {
         if (display_from_file) {
             glUseProgram(main_shader);
             {
+
+                glActiveTexture(GL_TEXTURE0);
+                glBindTexture(GL_TEXTURE_2D, placeholder_texture);
+
                 
-                GLint position = glGetAttribLocation(main_shader, "position");
-                GLint uvs = glGetAttribLocation(main_shader, "uvs");
-                GLint texture = glGetAttribLocation(main_shader, "texture_unit");
+                GLint position = glGetAttribLocation(main_shader, "position"); GL_ERR;
+                GLint uvs = glGetAttribLocation(main_shader, "uvs"); GL_ERR;
+                GLint texture = glGetAttribLocation(main_shader, "texture_unit"); GL_ERR;
 
                 glUniformMatrix4fv(glGetUniformLocation(main_shader, "view_matrix"),1,GL_FALSE,view_matrix);
                 glUniformMatrix4fv(glGetUniformLocation(main_shader, "projection_matrix"), 1, GL_FALSE, projection_matrix);
                 glUniform1f(texture, 0.0);
 
-                // @NOTE Texture loading from the ttf file gives a flipped texture which will certainly lead to incorrect results. Let's debug from a file written to disk for now.
-
-                //glBindTexture(GL_TEXTURE_2D, font_texture);
-                glBindTexture(GL_TEXTURE_2D, font_texture_from_file);
-                //glBindTexture(GL_TEXTURE_2D, test_texture);
 
                 int bytes_per_float = sizeof(float);
                 int stride = bytes_per_float * (5);
 
-                glEnableVertexAttribArray(position);
-                glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, stride, vertex_data);
-                glEnableVertexAttribArray(uvs);
-                glVertexAttribPointer(uvs, 2, GL_FLOAT, GL_FALSE, stride, vertex_data + 3);
+                if(vao_supported) {
+                    glBindVertexArray(mesh_vaoId);    
+                } else {
+                    int material_shader_LOC_VERTEX_POSITION = glGetAttribLocation(main_shader, "vertexPosition");
+                    int material_shader_LOC_VERTEX_TEXCOORD01 = glGetAttribLocation(main_shader, "vertexTexCoord");
 
-                glDrawArrays(GL_TRIANGLES, 0, 6);
+                     // Bind mesh VBO data: vertex position (shader-location = 0)
+                    glBindBuffer(GL_ARRAY_BUFFER, mesh_vboId[0]);
+                    glVertexAttribPointer(material_shader_LOC_VERTEX_POSITION, 3, GL_FLOAT, 0, 0, 0);
+                    glEnableVertexAttribArray(material_shader_LOC_VERTEX_POSITION);
+
+                    // Bind mesh VBO data: vertex texcoords (shader-location = 1)
+                    glBindBuffer(GL_ARRAY_BUFFER, mesh_vboId[1]);
+                    glVertexAttribPointer(material_shader_LOC_VERTEX_TEXCOORD01, 2, GL_FLOAT, 0, 0, 0);
+                    glEnableVertexAttribArray(material_shader_LOC_VERTEX_TEXCOORD01);
+                }
+
+                glDrawArrays(GL_TRIANGLES, 0, mesh_vertices_count);
+
+                glActiveTexture(GL_TEXTURE0);
+                glBindTexture(GL_TEXTURE_2D, 0);
+
             }
             glUseProgram(0);    
         }
